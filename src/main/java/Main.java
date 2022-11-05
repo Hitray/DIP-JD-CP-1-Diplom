055d6a04c995a805545d526cccfecc240040d35d
Hitray <75577433+Hitray@users.noreply.github.com>
2022-11-05 19:27:34 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
-
 public class Main {
     public static final int PORT = 8989;
+    private static final String PATH_PDF_FILE = "pdfs";
+    public static String STOP_WORDS_FILE_PATH = "stop-ru.txt";
 
     public static void main(String[] args) throws Exception {
 
-        SearchServer server = new SearchServer(PORT);
+        SearchServer server = new SearchServer(PORT, PATH_PDF_FILE);
         server.start();
     }
 }
